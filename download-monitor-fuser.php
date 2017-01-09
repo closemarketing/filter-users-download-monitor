a4a2befef5a63ba37701d59ea5415da45be9f489
David Perez <david@closemarketing.es>
2017-01-09 22:56:36 +0100

    adds metabox and shortcode

--- download-monitor-fuser.php
+++ download-monitor-fuser.php
@@ -15,6 +15,11 @@ Domain Path: /languages/
 
 License: GPL
 */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+
 class DMFUserPlugin {
 	/**
 	 * The plugin file
@@ -27,7 +32,88 @@ class DMFUserPlugin {
 	 */
 	public function __construct( $file ) {
 		$this->file = $file;
+
+    add_action('admin_init', array($this,'dmfu_admin_metabox') );
+    add_action('save_post', array($this,'dmfu_save_metabox') );
+    add_shortcode( 'download_user', array( $this, 'download_user' ) );
   }
+
+  /**
+  * Init for metabox
+  */
+  function dmfu_admin_metabox(){
+    add_meta_box('dmfu_users', __('Filter by user','dmfu'), array($this,'meta_options'), 'dlm_download', 'side', 'high');
+  }
+
+  /**
+  * Metbox Select user
+  */
+  function meta_options(){
+
+    $checkboxMeta = get_post_meta(get_the_id());
+
+    //* Gets the forms in array
+    $users = get_users();
+    echo '<p>'.__('Only this users will see this file:','dmfu').'</p>';
+    foreach( $users as $user ):
+        echo '<p><input type="checkbox" name="dmfu_userid_'.$user->ID.'" id="dmfu_userid_'.$user->ID.'" value="yes" ';
+        if ( isset ( $checkboxMeta['dmfu_userid_'.$user->ID] ) ) checked( $checkboxMeta['dmfu_userid_'.$user->ID][0], 'yes' );
+        echo ' />'.$user->display_name.'</p>';
+    endforeach;
+  }
+
+  function dmfu_save_metabox( $post_id ) {
+
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+        return;
+
+    //* Gets the forms in array
+    $users = get_users();
+    foreach( $users as $user ):
+      //saves checbox meta
+      if( isset( $_POST[ 'dmfu_userid_'.$user->ID ] ) ) {
+          update_post_meta( $post_id, 'dmfu_userid_'.$user->ID, 'yes' );
+      } else {
+          update_post_meta( $post_id, 'dmfu_userid_'.$user->ID, 'no' );
+      }
+    endforeach;
+
+  } //save metabox
+
+  /**
+  * download function for user.
+  *
+  * @access public
+  *
+  * @param array $atts
+  *
+  * @return string
+  */
+  function download_user()
+      $current_user = wp_get_current_user();
+
+      $args = array(
+      	'post_type'  => 'dlm_download',
+      	'meta_query' => array(
+      		array(
+      			'key'     => 'dmfu_userid_'.$current_user->ID,
+      			'value'   => 'yes',
+      			'compare' => 'IN',
+      		),
+      	),
+      );
+      $query = new WP_Query( $args );
+      if ( $query->have_posts() ) {
+      	while ( $query->have_posts() ) {
+          $query->the_post();
+          echo do_shortcode( '[download id="'.get_the_id().'"]' );
+      	}
+      	/* Restore original Post Data */
+      	wp_reset_postdata();
+      }
+  }
+
+
 } //from class
 
 global $dmfuser_plugin;
